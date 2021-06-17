0215e40c36f639517cf7f60ee8052589b330b795
Whatop <73518664+Whatop@users.noreply.github.com>
2021-06-17 17:04:51 +0900

    Temp++

--- Pixel-master/Space/Enemy.h
+++ Pixel-master/Space/Enemy.h
@@ -3,6 +3,15 @@ class Enemy : public Object
 {
 	Sprite* m_Enemy;
 	Sprite* m_ColBox[5];
+	//Idle Walk Die
+	
+	Sprite* m_Idle[3];
+	Sprite* m_Walk;
+	Sprite* m_Die;
+	
+	Sprite* m_Action;
+
+
 	float m_Hp, m_Speed;
 	Vec2 m_Dir;
 	bool m_Hit;
